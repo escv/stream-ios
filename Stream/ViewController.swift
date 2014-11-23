259bc085eafeb27c2ae4361d9cb7d4e5b60e2552
Andre Albert <andre.albert@prodyna.com>
2014-11-23 13:35:13 +0100

    initial Alamofire Test Request

--- Stream/ViewController.swift
+++ Stream/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import AlamofireSwiftyJSON
+
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request(.GET, "http://httpbin.org/get", parameters: ["foo": "bar"])
+            .responseSwiftyJSON { (request, response, json, error) in
+                println(json["headers"]["User-Agent"])
+        }
     }
 
     override func didReceiveMemoryWarning() {
